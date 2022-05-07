41e86f101b09cb857bff2e45114baf2ddfd7f19b
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-06 19:57:14 -0500

    Unit test for class user

--- Test/Models/User.test.js
+++ Test/Models/User.test.js
@@ -1,5 +1,14 @@
-describe("Prueba de Jest", () => {
-    test("Prueba 1", () => {
-        expect(10).toBe(9)
-    })
+const User = require('./../../App/Models/User')
+
+describe("Unit test for User class", () =>{
+    test('Create an User object', () => {
+        const user = new User(1,"LuisCorrea", "Luis", "Developer", "dateCreated", "lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("LuisCorrea")
+        expect(user.name).toBe("Luis")
+        expect(user.bio).toBe("Developer")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdates).toBe("lastUpdated")
+    });
 })
