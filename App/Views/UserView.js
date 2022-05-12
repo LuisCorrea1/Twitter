9cd42fe5a2375b0a7effa7b4a79425cb2f5216f2
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-11 23:42:59 -0500

    Requirement 2 - Test for Invalid properties and validation

--- App/Views/UserView.js
+++ App/Views/UserView.js
@@ -3,6 +3,9 @@ const UserService = require('./../../App/services/UserService')
 class UserView {
     static createUser(payload){
         if (payload === null) return {error: "payload no existe"}
+        if (payload.username != String || payload.name != String || payload.id != Number){
+            return {error: "Ingrese valores válidos"}
+        }
     }
     
 }
